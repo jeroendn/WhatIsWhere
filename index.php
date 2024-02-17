4bc9d6d8dbe2b85c5b01956f7d12617a9e4153c2
jeroendn <jeroendenijs.k.smile@outlook.com>
2024-02-17 19:48:00 +0100

    Scrape via public url
    Scrape via public url, since apps running in Docker cannot be read via shell anymore

--- index.php
+++ index.php
@@ -1,42 +1,74 @@
 <?php
 
-if (substr($_SERVER['HTTP_HOST'], -6) === '.local') {
-    echo 'App cannot run on localhost';
-    die;
-}
+$domains = [
+    'jeroendn.nl',
+//    'alpha.jeroendn.nl',
+    'beta.jeroendn.nl',
+    'gamma.jeroendn.nl',
+    'delta.jeroendn.nl',
+    'epsilon.jeroendn.nl',
+    'zeta.jeroendn.nl',
+    'eta.jeroendn.nl',
+    'theta.jeroendn.nl',
+    'iota.jeroendn.nl',
+    'kappa.jeroendn.nl',
+    'lambda.jeroendn.nl',
+    'mu.jeroendn.nl',
+    'nu.jeroendn.nl',
+    'xi.jeroendn.nl',
+    'omnicron.jeroendn.nl',
+    'pi.jeroendn.nl',
+    'rho.jeroendn.nl',
+    'sigma.jeroendn.nl',
+    'tau.jeroendn.nl',
+    'upsilon.jeroendn.nl',
+    'phi.jeroendn.nl',
+    'chi.jeroendn.nl',
+    'psi.jeroendn.nl',
+    'omega.jeroendn.nl',
+];
 
-$dirString = shell_exec('cd ../../ && ls');
+foreach ($domains as $domain) {
 
-$pattern = '/\b(?:https?:\/\/)?(?:[\w-]+\.)*\w[\w-]*(?:\.[a-z]{2,})+\b/';
-preg_match_all($pattern, $dirString, $matches);
-$urls = $matches[0];
+    $url = "https://$domain/composer.json"; // Replace with the actual URL of your JSON file
 
-?>
+    // Check if the file exists
+    $header_response = @get_headers($url, 1);
+    if (!isset($header_response[0]) || strpos($header_response[0], '404') !== false) {
+        echo 'File does not exist: ' . $url . '<br>';
+        continue;
+    }
 
-<?php foreach ($urls as $url): ?>
-    <?php if (!str_contains($url, 'jeroendn.nl')) continue; ?>
+    // Fetch JSON data from the URL
+    $jsonData = @file_get_contents($url);
+
+    // Check if the data was fetched successfully
+    if ($jsonData === false) {
+        echo 'Error fetching data from ' . $url . '<br>';
+        continue;
+    }
+
+    // Decode JSON data
+    $data = json_decode($jsonData, true); // The second parameter "true" makes it return an associative array
+
+    // Check if JSON decoding was successful
+    if ($data === null) {
+        echo 'Error decoding JSON data from ' . $url . '<br>';
+        continue;
+    }
+    ?>
     <details>
-        <summary><strong><?= $url ?></strong> (<?= $composerName = shell_exec(sprintf("cd ../../%s/public_html/ && php -r 'echo json_decode(file_get_contents(\"composer.json\"))->name;'", $url)); ?>)</summary>
-        <a href="https://<?= $url ?>" target="_blank">https://<?= $url ?></a>
-	<br>
-	<a href="http://<?= $url ?>" target="_blank">http://<?= $url ?></a>
+        <summary><strong><?= $domain ?></strong> (<?= $composerName = $data['name']; ?>)</summary>
+        <a href="https://<?= $domain ?>" target="_blank">https://<?= $domain ?></a>
+        <br>
+        <a href="http://<?= $domain ?>" target="_blank">http://<?= $domain ?></a>
         <div>
             <p>
                 <strong>Composer description:</strong><br>
                 <?= $composerName ?><br>
-                <?= shell_exec(sprintf("cd ../../%s/public_html/ && php -r 'echo json_decode(file_get_contents(\"composer.json\"))->description;'", $url)); ?>
-            </p>
-            <?php
-            $dirsString = shell_exec(sprintf('cd ../../%s/public_html/ && ls -A', $url));
-            $dirs       = preg_split('/\s+/', $dirsString);
-            ?>
-            <p>
-                <strong>Directories/Files</strong>
-                <br>
-                <?php foreach ($dirs as $dir): ?>
-                    <?= $dir ?><br>
-                <?php endforeach; ?>
+                <?= $data['description']; ?>
             </p>
         </div>
     </details>
-<?php endforeach; ?>
+    <?php
+}
