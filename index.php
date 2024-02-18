7066f082951802d295988198b4c2c03d3c99b051
jeroendn <jeroendenijs.k.smile@outlook.com>
2024-02-18 11:23:51 +0100

    Show internal Docker port numbers

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <?php
+/**
+ * Simple app to list an overview of currently hosted apps.
+ */
 
+// Indexes refer to the internal Docker port
 $domains = [
-    'jeroendn.nl',
-//    'alpha.jeroendn.nl',
-    'beta.jeroendn.nl',
-    'gamma.jeroendn.nl',
-    'delta.jeroendn.nl',
-    'epsilon.jeroendn.nl',
-    'zeta.jeroendn.nl',
-    'eta.jeroendn.nl',
-    'theta.jeroendn.nl',
-    'iota.jeroendn.nl',
-    'kappa.jeroendn.nl',
-    'lambda.jeroendn.nl',
-    'mu.jeroendn.nl',
-    'nu.jeroendn.nl',
-    'xi.jeroendn.nl',
-    'omnicron.jeroendn.nl',
-    'pi.jeroendn.nl',
-    'rho.jeroendn.nl',
-    'sigma.jeroendn.nl',
-    'tau.jeroendn.nl',
-    'upsilon.jeroendn.nl',
-    'phi.jeroendn.nl',
-    'chi.jeroendn.nl',
-    'psi.jeroendn.nl',
-    'omega.jeroendn.nl',
+    9000 => 'jeroendn.nl',
+    9001 => 'alpha.jeroendn.nl',
+    9002 => 'beta.jeroendn.nl',
+    9003 => 'gamma.jeroendn.nl',
+    9004 => 'delta.jeroendn.nl',
+    9005 => 'epsilon.jeroendn.nl',
+    9006 => 'zeta.jeroendn.nl',
+    9007 => 'eta.jeroendn.nl',
+    9008 => 'theta.jeroendn.nl',
+    9009 => 'iota.jeroendn.nl',
+    9010 => 'kappa.jeroendn.nl',
+    9011 => 'lambda.jeroendn.nl',
+    9012 => 'mu.jeroendn.nl',
+    9013 => 'nu.jeroendn.nl',
+    9014 => 'xi.jeroendn.nl',
+    9015 => 'omnicron.jeroendn.nl',
+    9016 => 'pi.jeroendn.nl',
+    9017 => 'rho.jeroendn.nl',
+    9018 => 'sigma.jeroendn.nl',
+    9019 => 'tau.jeroendn.nl',
+    9020 => 'upsilon.jeroendn.nl',
+    9021 => 'phi.jeroendn.nl',
+    9022 => 'chi.jeroendn.nl',
+    9023 => 'psi.jeroendn.nl',
+    9024 => 'omega.jeroendn.nl',
 ];
 
-foreach ($domains as $domain) {
+foreach ($domains as $port => $domain) {
 
     $url = "https://$domain/composer.json"; // Replace with the actual URL of your JSON file
 
@@ -67,6 +71,12 @@ foreach ($domains as $domain) {
                 <strong>Composer description:</strong><br>
                 <?= $composerName ?><br>
                 <?= $data['description']; ?>
+            </p>
+        </div>
+        <div>
+            <p>
+                <strong>Port:</strong><br>
+                <?= $port ?><br>
             </p>
         </div>
     </details>
